c47e2ae56a452cc194095dad44d40aae41315262
farhan071024 <mdfarhanhaque@gmail.com>
2015-11-03 22:40:56 -0600

    2nd update

--- app/src/main/java/com/apppartner/androidprogrammertest/AnimationActivity.java
+++ app/src/main/java/com/apppartner/androidprogrammertest/AnimationActivity.java
@@ -3,16 +3,19 @@ package com.apppartner.androidprogrammertest;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.widget.ImageView;
 
 
 public class AnimationActivity extends ActionBarActivity
 {
-
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animation);
+        imageView= (ImageView) findViewById(R.id.imageView2);
+        imageView.setImageResource(R.drawable.ic_apppartner);
     }
 
     @Override
--- app/src/main/java/com/apppartner/androidprogrammertest/adapters/ChatsArrayAdapter.java
+++ app/src/main/java/com/apppartner/androidprogrammertest/adapters/ChatsArrayAdapter.java
@@ -1,15 +1,22 @@
 package com.apppartner.androidprogrammertest.adapters;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.apppartner.androidprogrammertest.R;
 import com.apppartner.androidprogrammertest.models.ChatData;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
 
 /**
@@ -19,6 +26,8 @@ import java.util.List;
  */
 public class ChatsArrayAdapter extends ArrayAdapter<ChatData>
 {
+    ChatCell chatCell=null;
+    ChatData chatData=null;
     public ChatsArrayAdapter(Context context, List<ChatData> objects)
     {
         super(context, 0, objects);
@@ -26,19 +35,19 @@ public class ChatsArrayAdapter extends ArrayAdapter<ChatData>
 
     public View getView(int position, View convertView, ViewGroup parent)
     {
-        ChatCell chatCell = new ChatCell();
+         chatCell = new ChatCell();
 
         LayoutInflater inflater = LayoutInflater.from(getContext());
         convertView = inflater.inflate(R.layout.cell_chat, parent, false);
 
         chatCell.usernameTextView = (TextView) convertView.findViewById(R.id.usernameTextView);
         chatCell.messageTextView = (TextView) convertView.findViewById(R.id.messageTextView);
+        chatCell.imageView = (ImageView) convertView.findViewById(R.id.imageView);
 
+        chatData = getItem(position);
 
-        ChatData chatData = getItem(position);
-
-        chatCell.usernameTextView.setText(chatData.username);
-        chatCell.messageTextView.setText(chatData.message);
+        DownloadImage image= new DownloadImage();
+        image.execute(chatData.avatarURL);
 
         return convertView;
     }
@@ -47,5 +56,31 @@ public class ChatsArrayAdapter extends ArrayAdapter<ChatData>
     {
         TextView usernameTextView;
         TextView messageTextView;
+        ImageView imageView;
+    }
+    public class DownloadImage extends AsyncTask<String,Void,Bitmap>{
+        URL url = null;
+        Bitmap bmp=null;
+        @Override
+        protected Bitmap doInBackground(String... params) {
+
+            try {
+                url = new URL(params[0]);
+                bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream());
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return bmp;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            chatCell.imageView.setImageBitmap(bitmap);
+            chatCell.usernameTextView.setText(chatData.username);
+            chatCell.messageTextView.setText(chatData.message);
+
+        }
     }
 }
